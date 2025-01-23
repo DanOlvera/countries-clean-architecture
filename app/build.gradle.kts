a5e94056806a73cc1f823de45914740b05d1a313
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 14:27:15 -0800

    Updated compileSdk version to 35 required for one of the libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.countriescleanarchitecture"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.countriescleanarchitecture"
