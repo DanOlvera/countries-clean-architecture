3400d4920ecc3475f254e367eaac6c2f541bc88c
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 13:45:38 -0800

    Created the view in activity that will display the list of countries

--- app/src/main/java/com/example/countriescleanarchitecture/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/countriescleanarchitecture/presentation/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.countriescleanarchitecture
+package com.example.countriescleanarchitecture.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.countriescleanarchitecture.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
