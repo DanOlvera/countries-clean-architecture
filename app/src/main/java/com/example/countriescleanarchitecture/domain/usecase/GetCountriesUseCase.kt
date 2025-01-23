be2771ac7b6601fd2a2d81dbcb7a4c7a4035a4d4
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 13:20:07 -0800

    Updated GetCountriesUseCase to use flow

--- app/src/main/java/com/example/countriescleanarchitecture/domain/usecase/GetCountriesUseCase.kt
+++ app/src/main/java/com/example/countriescleanarchitecture/domain/usecase/GetCountriesUseCase.kt
@@ -1,10 +1,10 @@
 package com.example.countriescleanarchitecture.domain.usecase
 
 import com.example.countriescleanarchitecture.data.repository.CountryRepository
-import com.example.countriescleanarchitecture.domain.model.Country
+import com.example.countriescleanarchitecture.presentation.state.CountryState
+import kotlinx.coroutines.flow.Flow
 
 class GetCountriesUseCase(private val repository: CountryRepository) {
-    suspend fun invoke(): Result<List<Country>> {
-        return repository.getCountries()
-    }
+
+    suspend fun execute(): Flow<CountryState> = repository.getCountries()
 }
