c1d87d8a800a10b289ba86f20dda548b8f342efd
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 13:14:33 -0800

    Updated repository to use flow

--- app/src/main/java/com/example/countriescleanarchitecture/data/repository/CountryRepository.kt
+++ app/src/main/java/com/example/countriescleanarchitecture/data/repository/CountryRepository.kt
@@ -1,4 +1,8 @@
 package com.example.countriescleanarchitecture.data.repository
 
+import com.example.countriescleanarchitecture.presentation.state.CountryState
+import kotlinx.coroutines.flow.Flow
+
 interface CountryRepository {
+    suspend fun getCountries(): Flow<CountryState>
 }
