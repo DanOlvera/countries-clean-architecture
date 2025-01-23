c4177d420dc363b4007e64313602a28973b98d19
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 15:30:24 -0800

    Fixed dependencies conflicting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,14 +51,18 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     //To move to toml file
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation(libs.gson)
+    implementation(libs.converter.gson)
+
+
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.2")
+    implementation("androidx.compose.runtime:runtime:1.7.2")
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.10.0")
-    implementation ("androidx.recyclerview:recyclerview:1.3.1")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.7.2")
 
 }
