c373aefc27884bac5c789bb91caac180f85b6773
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 13:48:43 -0800

    Added required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //To move to toml file
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.10.0")
+    implementation ("androidx.recyclerview:recyclerview:1.3.1")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.7.2")
+
 }
