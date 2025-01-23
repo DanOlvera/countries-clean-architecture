b9257fd4d0f961e75d31820d508b568bae5a103e
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 12:39:21 -0800

    Added model data classes

---
+++ app/src/main/java/com/example/countriescleanarchitecture/domain/model/Currency.kt
@@ -0,0 +1,9 @@
+package com.example.countriescleanarchitecture.domain.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Currency(
+    @SerializedName("code"   ) var code   : String? = null,
+    @SerializedName("name"   ) var name   : String? = null,
+    @SerializedName("symbol" ) var symbol : String? = null
+)
