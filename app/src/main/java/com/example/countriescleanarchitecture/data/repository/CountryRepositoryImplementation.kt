836ba53e64376915293c3f7481e30fb7fcfafd55
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 13:21:57 -0800

    Uupdated CountryRepositoryImplemetation to use flow instead of Result

--- app/src/main/java/com/example/countriescleanarchitecture/data/repository/CountryRepositoryImplementation.kt
+++ app/src/main/java/com/example/countriescleanarchitecture/data/repository/CountryRepositoryImplementation.kt
@@ -1,4 +1,21 @@
 package com.example.countriescleanarchitecture.data.repository
 
-class CountryRepositoryImplementation {
+import com.example.countriescleanarchitecture.data.datasource.CountryApi
+import com.example.countriescleanarchitecture.presentation.state.CountryState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class CountryRepositoryImplementation(private val countryApi: CountryApi): CountryRepository {
+
+    override suspend fun getCountries(): Flow<CountryState> = flow {
+        emit(CountryState.Loading)
+        try {
+            val response = countryApi.getCountries()
+            emit(CountryState.Success(response))
+        } catch (e: Exception) {
+            emit(CountryState.Error(e.message ?: "Unknown error"))
+        }
+    }.flowOn(Dispatchers.IO)
 }
