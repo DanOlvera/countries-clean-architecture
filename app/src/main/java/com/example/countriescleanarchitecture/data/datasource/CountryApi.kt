16166c192f782aa61261fd8bef1bb4924bd0d0d2
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-23 13:00:46 -0800

    Added api code to fetch data from remote using retrofit

--- app/src/main/java/com/example/countriescleanarchitecture/data/datasource/CountryApi.kt
+++ app/src/main/java/com/example/countriescleanarchitecture/data/datasource/CountryApi.kt
@@ -1,4 +1,9 @@
 package com.example.countriescleanarchitecture.data.datasource
 
+import com.example.countriescleanarchitecture.domain.model.Country
+import retrofit2.http.GET
+
 interface CountryApi {
+    @GET("countries.json")
+    suspend fun getCountries(): List<Country>
 }
---
+++ app/src/main/java/com/example/countriescleanarchitecture/data/datasource/CountryApiService.kt
@@ -0,0 +1,26 @@
+package com.example.countriescleanarchitecture.data.datasource
+
+import com.example.countriescleanarchitecture.data.utils.ApiUtils
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object CountryApiService {
+
+    private val logging = HttpLoggingInterceptor().apply {
+        setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    private val okHttpClient = OkHttpClient.Builder().apply {
+        addInterceptor(logging)
+    }
+
+    val apiService: CountryApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(ApiUtils.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CountryApi::class.java)
+    }
+}
