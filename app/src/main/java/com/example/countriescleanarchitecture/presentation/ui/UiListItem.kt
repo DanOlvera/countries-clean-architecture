0fc3f9d2caa4d8e42780f13c51775d96de1660bd
DanOlvera <ing.josuedanielescamilla@gmail.com>
2025-01-30 20:12:27 -0800

    Completed Headers from challenge

--- app/src/main/java/com/example/countriescleanarchitecture/presentation/ui/UiListItem.kt
+++ app/src/main/java/com/example/countriescleanarchitecture/presentation/ui/UiListItem.kt
@@ -1,4 +1,9 @@
 package com.example.countriescleanarchitecture.presentation.ui
 
-interface UiListItem {
+import com.example.countriescleanarchitecture.domain.model.Country
+
+sealed interface UiListItem {
+    data class CountryListItem(val country: Country): UiListItem
+    data class HeaderListItem(val header: String): UiListItem
+    // data class Footer
 }
--- app/src/main/java/com/example/countriescleanarchitecture/presentation/viewmodel/CountryViewModel.kt
+++ app/src/main/java/com/example/countriescleanarchitecture/presentation/viewmodel/CountryViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.countriescleanarchitecture.presentation.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.countriescleanarchitecture.domain.model.Country
 import com.example.countriescleanarchitecture.domain.usecase.GetCountriesUseCase
 import com.example.countriescleanarchitecture.presentation.state.CountryState
+import com.example.countriescleanarchitecture.presentation.ui.UiListItem
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -19,5 +22,29 @@ class CountryViewModel (private val getCountriesUseCase: GetCountriesUseCase): V
                 _state.value = emittedState
             }
         }
+    }
+
+    fun getUiListItem(countries: List<Country>) : List<UiListItem> {
+        val uiList = mutableListOf<UiListItem>()
+        //First character
+        ///Store -> header
+        //Iterate -> from until the first character changes
+        //B ca new header and i
+        //
+        var previous: Char? = null
+        for(c in countries) {
+            val currentFirstLetter = c.name?.toCharArray()?.first()
+
+            if (previous == null || previous != currentFirstLetter) {
+                // add header
+                uiList.add(UiListItem.HeaderListItem(currentFirstLetter.toString()))
+            }
+            // add country
+            uiList.add(UiListItem.CountryListItem(c))
+            previous = currentFirstLetter
+
+        }
+
+        return uiList
     }
 }
